d08037dc6590cb3d562d4d1e254feafd1567b251
Nedko Hristov <nedelcho.hristov2001@gmail.com>
2016-07-20 01:27:10 +0300

    variable domain changed
    variable domain changed to domainName to test github commits

--- URLCheck.py
+++ URLCheck.py
@@ -1,12 +1,12 @@
 import requests # import library "requests"
-domain = 'http://qualityassurance.pro' #Let's set the domain in variable "domain"
-request = requests.get(domain) #set requests.get to open the existing domain https://nedko.info
+domainName = 'http://qualityassurance.pro' #Let's set the domain in variable "domainName"
+request = requests.get(domainName) #set requests.get to open the existing domain https://nedko.info
 if request.status_code == 200: #If the resource return status code 200 (success)
-    print('Woohoo - the web site ',domain,' exists') #Print success message
+    print('Woohoo - the web site ',domainName,' exists') #Print success message
 else: #or
-    print('Not cool - the web site ',domain,' seems that does not exist') #Print unsuccessful message
+    print('Not cool - the web site ',domainName,' seems that does not exist') #Print unsuccessful message
 print ("") #Print empty line
-print('Status code: ',request.status_code) #Print returned status code here
+print('Status code: ',request.status_code) #Print returned status code here 
 
 
 # Expected result:
